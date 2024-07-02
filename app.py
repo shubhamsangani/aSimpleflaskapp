93fb8481aece2c9dbaf9c3ccc76e0244e3ac2394
shubhamsangani3572 <shubhamsangani3572@gmail.com>
2024-07-02 17:07:21 +0530

    new app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<center><h1>FLASK APP DEPLOY ON MICROSOFT AZURE</h2></center>"
+    return "<center><h1>FLASK APP DEPLOY ON MICROSOFT AZURE AND BUILD THIS FLASK APP TO REPRESENT CI/CD PIPELINE</h2></center>"
 
 if __name__ == "__main__":
     app.run()
